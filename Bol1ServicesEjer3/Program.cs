e5ef1378d6cf95039fbd4fa0ecdcae50ebfff165
Iris SF <iris.kosy@gmail.com>
2020-11-09 17:25:45 +0100

    Ejercicio validado

--- Bol1ServicesEjer3/Program.cs
+++ Bol1ServicesEjer3/Program.cs
@@ -11,41 +11,88 @@ namespace Bol1ServicesEjer3
     class Program
     {
         public delegate int Operation(int a);
+        public static bool flag = true;
 
         static readonly private object l = new object(); //declaramos a nuestra llave maestra
         static void Main(string[] args)
         {
             int num = 0;
-            Operation op = (a) => a + 1;
-            new Thread((a) =>
+            Operation op;
+            Thread thread1 = new Thread(() =>
             {
                 Operation op = (a) => a - 1;
-                while (num > -1000)
+                while (flag)
                 {
-                    lock(l){
-                        num = op(num);
-                        Console.SetCursorPosition(1, 2);
-                        Console.WriteLine("Thread 2:{0}", num);
+                    lock (l)
+                    {
+                        if (flag)
+                        {
+                            num = op(num);
+                            Console.WriteLine("Thread 2:{0}", num);
+                            if (num == -100)
+                            {
+                                flag = false;
+                            }
+                        }
+
                     }
-                    
+
                 }
 
-            }).Start();
-            while (num < 1000)
+            });
+
+            Thread thread2 = new Thread(() =>
             {
-                lock (l)
+                Operation op = (a) => a + 1;
+                while ( flag)
                 {
-                    num = op(num);
-                    Console.SetCursorPosition(1, 1);
-                    Console.WriteLine("Thread 1:{0}", num);
+                    lock (l)
+                    {
+                        if (flag)
+                        {
+                            num = op(num);
+                            Console.WriteLine("Thread 1:{0}", num);
+                            if (num == 100)
+                            {
+                                flag = false;
+                            }
+                        }
+
+                    }
+
                 }
-                
-            }
 
-            
+            });
+            thread1.Start();
+            thread2.Start();
 
+            thread1.Join(); // podemos apañar con uno ya que se paran los dos si uno gana
+
+            if (num > 0) Console.WriteLine("Gana hilo 1");
+            else Console.WriteLine("Gana hilo 2");
             Console.ReadLine();
+            
+            
         }
 
     }
 }
+
+
+//while (num < 1000 && flag)
+//{
+//    lock (l)
+//    {
+//        if (flag)
+//        {
+//            num = op(num);
+//            Console.WriteLine("Thread 1:{0}", num);
+//            if (num == 1000)
+//            {
+//                flag = false;
+//            }
+//        }
+
+//    }
+
+//}
